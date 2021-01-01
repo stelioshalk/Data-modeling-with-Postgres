9fc77a8c8374558487083bae6c56c948b8df2373
stelioshalk <49649484+stelioshalk@users.noreply.github.com>
2021-01-01 18:58:51 +0000

    2nd review additional fix

--- sql_queries.py
+++ sql_queries.py
@@ -41,7 +41,7 @@ values(%s,%s,%s,%s,%s,%s,%s,%s)""")
 
 user_table_insert = ("""INSERT INTO users 
 (user_id, first_name, last_name, gender, level) 
-VALUES (%s,%s,%s,%s,%s) ON CONFLICT (user_id) DO NOTHING""")
+VALUES (%s,%s,%s,%s,%s) ON CONFLICT (user_id) DO UPDATE SET level='paid'""")
 
 song_table_insert = (""" 
 INSERT INTO songs ("song_id", "title", "artist_id", "year", "duration") 
